1ffd0b50eb6ee5d89eb4447280ae73d303eb8182
Shan Siddiqui <shansiddiqui94@gmail.com>
2020-06-11 17:54:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if(cardNumber){ // is cardNumber a truthy value
     var Msg = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`//empty out cart?
-    
+    setCart([])
   }
   return "Sorry, we don't have a credit card on file for you."
 }
